2d8c2b01e4d0604644b8a87291597cf7184be462
Matheus Alves <theus.alvesn@gmail.com>
2025-03-24 16:59:27 -0300

    fix npm start build

--- src/server/config/database.ts
+++ src/server/config/database.ts
@@ -1,10 +1,15 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
-
 import path from "path";
+import fs from "fs";
 
 const dataBaseDir = path.resolve(__dirname, "../database");
+
 const dbPath = path.resolve(dataBaseDir, "database.db");
+
+if (!fs.existsSync(dataBaseDir)) {
+  fs.mkdirSync(dataBaseDir, { recursive: true });
+}
 
 export async function openDb() {
   return open({
--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -11,59 +11,6 @@ const router = Router();
 
 router.post('/user/signup', validateSignUp, handleValidationErrors, userHandler.signUp);
 router.post('/user/signin', validateSignIn, handleValidationErrors, userHandler.signInController);
-
-/** 
- * @swagger
- * /user/favorites:
- *   post:
- *     summary: Adiciona um personagem aos favoritos do usuário
- *     description: Esta rota permite que o usuário adicione um personagem à sua lista de favoritos. Requer autenticação e validação para garantir que o usuário não ultrapasse o limite de favoritos.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               characterId:
- *                 type: integer
- *                 description: ID do personagem a ser adicionado aos favoritos.
- *                 example: 242
- *     responses:
- *       201:
- *         description: Personagem adicionado aos favoritos com sucesso.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: true
- *                 message:
- *                   type: string
- *                   example: "Character added to favorites"
- *                 data:
- *                   type: object
- *                   properties:
- *                     characterId:
- *                       type: integer
- *                       example: 242
- *       400:
- *         description: Bad Request - Se o corpo da requisição estiver mal formado.
- *       401:
- *         description: Unauthorized - Se o usuário não estiver autenticado.
- *       404:
- *         description: Not Found - Se o usuário não for encontrado.
- *       409:
- *         description: Conflict - Se o personagem já estiver nos favoritos.
- *       403:
- *         description: Forbidden - Se o usuário tiver atingido o limite de favoritos.
- *       500:
- *         description: Internal Server Error - Se ocorrer um erro no servidor.
- */
-
-
 router.post('/user/favorites', authenticateToken, validateAddFavorites, handleValidationErrors, favoritesController.addToFavoritesHandler);
 router.get('/user/favorites', authenticateToken, favoritesController.getFavoritesCharactersHandler);
 router.get('/user/favorites/episodes', authenticateToken, favoritesController.getEpisodesPerFavoriteHandler);
@@ -75,3 +22,6 @@ router.get('/characters/search/:name', authenticateTokenOptional, countUserReque
 
 
 export { router };
+
+
+
